1e4d5ede2d99ed9a3169bdcf6e327c9590168494
Nicole Ignasiak <nicole.ignasiak@myemail.indwes.edu>
2015-10-14 16:40:42 +0000

    UPDATED week five assignment

---
+++ GameOfWar.2.py
@@ -0,0 +1,92 @@
+# Nicole Ignasiak
+# 10/7/15
+# CIS 125 82A
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+GameOfWar.py
+
+Created by Neumann, Daniel on 2015-10-06.
+Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+
+This is the shell copy. Fill this out to get it to work
+
+"""
+
+import random	
+
+def main():
+	"""
+	Deck, PlayerAHand and PlayerBHand are all lists
+	"""
+	
+	Deck = []
+	PlayerAHand = []
+	PlayerBHand = []
+	gameCounter = 0
+
+	# Create deck.  Cards are represented by an integer value
+	for i in range(52):
+		Deck.append(i)
+	
+	# Shuffle the deck
+	random.shuffle(Deck)
+	
+	# Deal 1/2 the cards to each player
+	for x in range(26):
+		PlayerAHand.append(Deck.pop())
+		PlayerBHand.append(Deck.pop())
+	
+	# Main Gameplay
+		
+	while len(PlayerAHand) > 0 and len(PlayerBHand) > 0:
+		gameCounter += 1
+		PlayerAHand, PlayerBHand = playRound(PlayerAHand, PlayerBHand)
+
+	# End of game
+	
+	print("There were ", gameCounter, " rounds played")
+	
+def playRound(PlayerA, PlayerB):
+	AsCard = PlayerA.pop()
+	BsCard = PlayerB.pop()
+	
+	AsRank = getRank(AsCard)
+	BsRank = getRank(BsCard)
+	
+	if AsRank > BsRank:
+		PlayerA.insert(0, AsCard)
+	elif BsRank > AsRank:
+		PlayerB.insert(0, BsCard)
+	else:
+		PlayerA, PlayerB = WAR(PlayerA, PlayerB)
+		
+	'''
+	This is the method that plays one round of War
+	The method takes PlayerA and PlayerB as input parameters
+	and returns PlayerA and PlayerB after modification
+	for the round
+	
+	Remember, high card wins. I have included a convenience
+	function getRank(anyCard) that will return the rank.
+	
+	See the README.md for the variations of
+	the game to program.
+	'''
+	return PlayerA, PlayerB
+
+
+def WAR(PlayerA, PlayerB):
+	# See the README.md file for instructions on coding 
+	# This module.
+
+	return PlayerA, PlayerB
+
+	
+def getRank(anyCard):
+	return anyCard % 13
+
+
+if __name__ == '__main__':
+	main()
+
---
+++ GameOfWar.3.py
@@ -0,0 +1,114 @@
+# Nicole Ignasiak
+# 10/7/15
+# CIS 125 82A
+# In collaboration with Trevor Waite, Josiah Hardacre, and Nathan Pugh
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+GameOfWar.py
+
+Created by Neumann, Daniel on 2015-10-06.
+Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+
+This is the shell copy. Fill this out to get it to work
+
+"""
+
+import random	
+
+def main():
+	"""
+	Deck, PlayerAHand and PlayerBHand are all lists
+	"""
+	
+	Deck = []
+	PlayerAHand = []
+	PlayerBHand = []
+	gameCounter = 0
+
+	# Create deck.  Cards are represented by an integer value
+	for i in range(52):
+		Deck.append(i)
+	
+	# Shuffle the deck
+	random.shuffle(Deck)
+	
+	# Deal 1/2 the cards to each player
+	for x in range(26):
+		PlayerAHand.append(Deck.pop())
+		PlayerBHand.append(Deck.pop())
+	
+	# Main Gameplay
+		
+	while len(PlayerAHand) > 0 and len(PlayerBHand) > 0:
+		gameCounter += 1
+	# Stops game if it plays more than 1000 rounds
+		if gameCounter > 999:
+			print ("Game is a draw")
+			break
+		PlayerAHand, PlayerBHand = playRound(PlayerAHand, PlayerBHand)
+
+	# End of game
+	
+	print("There were ", gameCounter, " rounds played")
+	
+def playRound(PlayerA, PlayerB):
+	'''
+	This is the method that plays one round of War
+	The method takes PlayerA and PlayerB as input parameters
+	and returns PlayerA and PlayerB after modification
+	for the round
+	
+	Remember, high card wins. I have included a convenience
+	function getRank(anyCard) that will return the rank.
+	
+	See the README.md for the variations of
+	the game to program.
+	'''
+	
+	AsCard = PlayerA.pop()
+	BsCard = PlayerB.pop()
+	
+	AsRank = getRank(AsCard)
+	BsRank = getRank(BsCard)
+	
+	if AsRank > BsRank:
+		PlayerA.insert(0, AsCard)
+		PlayerA.insert(0, BsCard)
+	elif BsRank > AsRank:
+		PlayerB.insert(0, AsCard)
+		PlayerB.insert(0, BsCard)
+	else:
+		PlayerA.append(AsCard)
+		PlayerB.append(BsCard)
+		PlayerA, PlayerB = WAR(PlayerA, PlayerB)
+	return PlayerA, PlayerB
+
+
+def WAR(PlayerA, PlayerB):
+	if len(PlayerA) > 5 and len(PlayerB) > 5:
+		AsHand = []
+		BsHand = []
+		for y in range(5):
+			AsHand.append(PlayerA.pop())
+			BsHand.append(PlayerB.pop())
+
+		if getRank(AsHand[4]) > getRank(BsHand[4]):
+			PlayerA = AsHand + BsHand + PlayerA
+		elif getRank(AsHand[4]) > getRank(BsHand[4]):
+			PlayerB =  AsHand + BsHand + PlayerB
+		else:
+			PlayerA, PlayerB = Lose(PlayerA, PlayerB)
+	return PlayerA, PlayerB
+
+def Lose(PlayerA, PlayerB):
+	#lose cards
+	return PlayerA, PlayerB
+	
+def getRank(anyCard):
+	return anyCard % 13
+
+
+if __name__ == '__main__':
+	main()
+
